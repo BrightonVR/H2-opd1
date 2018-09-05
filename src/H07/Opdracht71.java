bb50e79ddc771f53946b905d398efbd50c97996c
BrightonVR <bvanrouendal@roc-dev.com>
2018-09-05 13:23:19 +0200

    H7 Opdrachten

---
+++ src/H07/Opdracht71.java
@@ -0,0 +1,15 @@
+package H07;
+
+import java.applet.*;
+import java.awt.*;
+
+public class Opdracht71 extends Applet {
+
+    public void init(){
+        setSize(600,400);
+    }
+
+    public void paint(Graphics g){
+        g.drawString("Welcome to Java!!", 50, 60 );
+    }
+}
