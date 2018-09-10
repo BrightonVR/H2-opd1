96019a44e68590fe97a00f2f64aa22678441747e
BrightonVR <bvanrouendal@roc-dev.com>
2018-09-10 08:36:41 +0200

    H10 Alle opdrachten

---
+++ src/H10/H10Test.java
@@ -0,0 +1,11 @@
+package H10;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H10Test extends Applet {
+    public void init() {
+    }
+    public void paint(Graphics g) {
+    }
+}
